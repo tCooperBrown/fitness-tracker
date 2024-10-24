bbff79127425011d187f235bcadd0bbc1993ef07
Theo Cooper-Brown <trcb.github@gmail.com>
2024-10-24 15:36:41 +0100

    feat(goal route): zod input validation, error & success standardisation
    Applicable to goal creation [POST /api/goal] & goal updates [PUT /api/goal]:
    - Implemented zod input validation.
    - Implemented db transactions to prevent inconsistencies
    
    Applicable to goal creation [POST /api/goal] & goal updates [PUT /api/goal] & goal retrieval [GET /api/goal]:
    - Wrapped endpoints behind asyncHandler() for normalised error handling.
    - Standardised success response objects

--- routes/goal.js
+++ routes/goal.js
@@ -1,75 +1,130 @@
 import express from "express";
 import { isAuthenticated } from "../middleware/authMiddleware.js";
 import { db } from "../dbConnection.js";
-const router = express.Router();
+import { asyncHandler } from "../middleware/errorMiddleware.js";
+import { z } from "zod";
+import { AppError } from "../Types/errorTypes.js";
 
-router.post("/api/goal", isAuthenticated, async (req, res, next) => {
-  try {
-    const { goalType, startDate, goalWeight, userId } = req.body;
-
-    const goal = await db("goal")
-      .insert({ goalType, startDate, goalWeight, userId })
-      .returning("*");
-
-    res.status(201).json({
-      message: `Goal successfully created for userId: ${req.user.id}`,
-      goal,
-    });
-  } catch (error) {
-    // Implement error logging middleware later.
-    error.code = 500;
-    console.error(error);
-    res.status(error.code).json({ message: "Error creating new goal." });
-  }
+// 1. Dropped support for explicitly specifiying start date as intended behaviour is to
+// create a new goal on the actual start date.
+// 2. Will alter db schema in upcoming commit.
+const baseGoalSchema = z.object({
+  goalType: z.enum(["loss", "gain", "maintain"]),
+  goalWeight: z
+    .number()
+    .positive("Goal weight must be a positive number")
+    .max(1200, "Goal weight cannot exceed 1200kg"),
 });
 
-router.get("/api/goal", isAuthenticated, async (req, res, next) => {
-  try {
+const goalWithIdSchema = baseGoalSchema.extend({
+  goalId: z
+    .number("goalId must be a number")
+    .positive("goalId must be positive"),
+});
+
+const router = express.Router();
+
+router.post(
+  "/api/goal",
+  isAuthenticated,
+  asyncHandler(async (req, res, next) => {
+    const validatedData = await baseGoalSchema
+      .parseAsync(req.body)
+      .catch((err) => {
+        throw new AppError(err.errors[0].message, 400);
+      });
+
+    // devNote: need to confirm string formatting. Create test to see if this works
+    // e.g. suffix "*.930Z" likely cut off when serialised in db.
+    const todayISOString = new Date().toISOString();
+
+    const existingGoal = await db("goal")
+      .where({
+        userId: req.user.id,
+        startDate: todayISOString,
+      })
+      .first();
+
+    if (existingGoal) {
+      throw new AppError("Goal already exists for this date", 409);
+    }
+
+    const [goal] = await db.transaction((trx) =>
+      trx("goal")
+        .insert({
+          userId: req.user.id,
+          startDate: todayISOString,
+          goalType: validatedData.goalType,
+          goalWeight: validatedData.goalWeight,
+        })
+        .returning("*")
+    );
+
+    res.status(201).json({
+      status: "success",
+      message: "Goal successfully created.",
+      data: { goal },
+    });
+  })
+);
+
+router.get(
+  "/api/goal",
+  isAuthenticated,
+  asyncHandler(async (req, res, next) => {
     const userId = req.user.id;
     const goalEntries = await db.select().from("goal").where({ userId });
 
-    res.status(200).json({ goalEntries });
-  } catch (error) {
-    // Implement error logging middleware later.
-    error.code = 500;
-    console.error(error);
-    res.status(error.code).json({ message: "Error retrieving goal entries." });
-  }
-});
+    res.status(200).json({
+      status: "success",
+      message: "Goal entries retrieved.",
+      data: { goalEntries },
+    });
+  })
+);
 
-router.put("/api/goal", isAuthenticated, async (req, res, next) => {
-  try {
+router.put(
+  "/api/goal",
+  isAuthenticated,
+  asyncHandler(async (req, res, next) => {
     const userId = req.user.id;
-    const { goalId, goalType, goalWeight } = req.body;
+
+    const validatedData = await goalWithIdSchema
+      .parseAsync(req.body)
+      .catch((err) => {
+        throw new AppError(err.errors[0].message, 400);
+      });
 
     const requestedGoal = await db
       .select()
       .from("goal")
-      .where({ userId, id: goalId })
+      .where({ userId, id: validatedData.goalId })
       .first();
 
     if (!requestedGoal) {
-      return res.status(404).json({ message: "Goal not found" });
+      throw new AppError(
+        "Specified goal could not be found. Goal update was not attempted.",
+        404
+      );
     }
 
-    const updatedGoal = await db("goal")
-      .where({ userId, id: goalId })
-      .update({
-        goalType: goalType || requestedGoal.goalType,
-        goalWeight: goalWeight || requestedGoal.goalWeight,
-      })
-      .returning("*");
+    const [updatedGoal] = await db.transaction((trx) =>
+      trx("goal")
+        .where({ userId, id: validatedData.goalId })
+        .update({
+          goalType: validatedData.goalType || requestedGoal.goalType,
+          goalWeight: validatedData.goalWeight || requestedGoal.goalWeight,
+        })
+        .returning("*")
+    );
 
-    res.status(201).json({
-      message: "Goal updated successfully",
-      goal: updatedGoal[0],
+    res.status(200).json({
+      status: "success",
+      message: "Goal updated successfully.",
+      data: { updatedGoal },
     });
-  } catch (error) {
-    error.code = 500;
-    console.error(error);
-    res.status(error.code).json({ message: "Error retrieving goal entries." });
-  }
-});
+  })
+);
 
 router.delete("/api/goal/:goalId", isAuthenticated, async (req, res, next) => {
   try {
